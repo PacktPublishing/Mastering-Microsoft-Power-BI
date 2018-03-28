7f527ae6aed8c6cbb82446117a2ce6722e30b5b6
snehah7 <snehahanchate7@gmail.com>
2018-03-28 18:20:04 +0530

    Add files via upload

---
+++ Chapter12/PowerShell/Audit Log Search.ps1
@@ -0,0 +1,9 @@
+﻿$UserCredential = Get-Credential
+$CurrentDate = get-date
+$Yesterday = $CurrentDate.AddDays(-1)
+$csvFile = "C:\Users\Brett Powell\Desktop\PowerBIAuditLogs.csv"
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+ 
+Import-PSSession $Session
+
+$result = Search-UnifiedAuditLog -StartDate $Yesterday -EndDate $CurrentDate -RecordType PowerBI -ResultSize 5000 | Export-Csv $csvFile 
